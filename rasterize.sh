279466253f4bfb22bdbe7cb25b031e3a6239e5f0
Matthew Dillon <matthewrdillon@gmail.com>
2016-10-21 16:22:36 -0700

    ENH: macOS logos

--- rasterize.sh
+++ rasterize.sh
@@ -2,7 +2,7 @@
 
 ZIP=qiime2_logos_v4
 
-rm -rf raster && mkdir raster
+rm -rf raster && mkdir -p raster/apple_iconset
 
 svgexport vector/qiime2-square.svg raster/qiime2-square-400.png
 svgexport vector/qiime2-square.svg raster/qiime2-square-200.png 200:
@@ -17,6 +17,18 @@ convert raster/qiime2-square-400.png raster/qiime2-square-400.tiff
 convert raster/qiime2-square-600.png raster/qiime2-square-600.tiff
 convert raster/qiime2-square-800.png raster/qiime2-square-800.tiff
 convert raster/qiime2-square-1000.png raster/qiime2-square-1000.tiff
+
+svgexport vector/qiime2-square.svg raster/apple_iconset/icon_16x16.png 16:
+svgexport vector/qiime2-square.svg raster/apple_iconset/icon_32x32.png 32:
+svgexport vector/qiime2-square.svg raster/apple_iconset/icon_128x128.png 128:
+svgexport vector/qiime2-square.svg raster/apple_iconset/icon_256x256.png 256:
+svgexport vector/qiime2-square.svg raster/apple_iconset/icon_512x512.png 512:
+
+cp raster/apple_iconset/icon_16x16.png raster/apple_iconset/icon_16x16@2x.png
+cp raster/apple_iconset/icon_32x32.png raster/apple_iconset/icon_32x32@2x.png
+cp raster/apple_iconset/icon_128x128.png raster/apple_iconset/icon_128x128@2x.png
+cp raster/apple_iconset/icon_256x256.png raster/apple_iconset/icon_256x256@2x.png
+cp raster/apple_iconset/icon_512x512.png raster/apple_iconset/icon_512x512@2x.png
 
 svgexport vector/qiime2.svg raster/qiime2-rect-400.png
 svgexport vector/qiime2.svg raster/qiime2-rect-200.png 200:
